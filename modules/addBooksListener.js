256da64cb76dc433d72980280144400757f88241
Rafael Guzman <rega1237@gmail.com>
2022-04-26 08:57:58 -0400

    Reset input fields when the book is added and text 'new book added'

--- modules/addBooksListener.js
+++ modules/addBooksListener.js
@@ -4,8 +4,15 @@ import displayBooks from './displayBooks.js';
 
 export default () => {
   addBookBtn.addEventListener('click', () => {
+    const form = document.querySelector('.add-book-form');
+    const addBookText = document.querySelector('.new-book-title')
     const book = new Book(titleInput.value, authorInput.value);
     book.add(book);
     displayBooks();
+    form.reset();
+    addBookText.textContent = 'New Book Added';
+    setTimeout(() => {
+      addBookText.textContent = 'Add a new book';
+    }, 2000);
   });
 };
